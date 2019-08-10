acc12367680a083407cff0cb6af834c0c58980bf
Matt <mattsully01@gmail.com>
2019-08-10 08:43:43 -0600

    first commit

---
+++ app/server.js
@@ -0,0 +1,21 @@
+
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function(){
+    console.log("App is listening on PORT: " + PORT)
+});
+
+
