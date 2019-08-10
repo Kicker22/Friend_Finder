f4e2e032e1877c9c7251455b88bbda9b5f5ac452
Matt <mattsully01@gmail.com>
2019-08-10 17:30:19 -0600

    second commit

--- app/server.js
+++ app/server.js
@@ -10,12 +10,15 @@ var PORT = process.env.PORT || 8080
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function(){
     console.log("App is listening on PORT: " + PORT)
 });
+
+
+
 
 
